2700032f1bdcb2e9e74ab220473b283cf1604495
barmaj-co <barmaj.co@gmail.com>
2024-02-19 00:42:15 +0200

    add footer

---
+++ assets/js/footer.js
@@ -0,0 +1,26 @@
+(function () {
+    let footer = drawFooterElement();
+    document.getElementById("footer").appendChild(footer);
+})();
+
+function drawFooterElement() {
+    let divElement = document.createElement("div");
+    divElement.className += `container py-4 text-center`;
+
+    let textElement = document.createElement("div");
+    textElement.className += `text-center`;
+
+    let copyElement = document.createElement("div");
+    copyElement.className += `copyright`;
+    copyElement.innerHTML += "جميع الحقوق محفوظة &copy; تراويح رمضان 1445  ";
+    textElement.appendChild(copyElement);
+
+    let nameElement = document.createElement("div");
+    nameElement.className += `credits`;
+    nameElement.innerHTML += "تصميم م.اسلام هيكل";
+    textElement.appendChild(nameElement);
+
+    divElement.appendChild(textElement);
+
+    return divElement;
+}
