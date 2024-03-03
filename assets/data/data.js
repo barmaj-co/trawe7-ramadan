90cf8e284b4fe7e8d376fe5e224089443e0d62cc
barmaj-co <59221358+barmaj-co@users.noreply.github.com>
2024-03-03 20:22:05 +0200

    update mosues

--- assets/data/data.js
+++ assets/data/data.js
@@ -1,8 +1,25 @@
 var mosques = [
-    { id: 1, name: 'التوحيد', address: 'بدواى - الميدان', youtubeUrl: '', imageUrl: '' },
+    { id: 1, name: 'المساكن', address: 'بدواى - بجوار نادى شبان بدواى', youtubeUrl: '', imageUrl: '' },
     { id: 2, name: 'التسويق', address: 'بدواي - بجوار الثلاجة', youtubeUrl: '', imageUrl: '' },
-    { id: 3, name: 'عبدالرحمن', address: '', youtubeUrl: '', imageUrl: '' },
-    { id: 4, name: 'الكبير', address: '', youtubeUrl: '', imageUrl: '' },
+    { id: 3, name: 'عائشة رياض', address: 'بدواى - بجوار الحاج صبرى', youtubeUrl: '', imageUrl: '' },
+    { id: 4, name: 'عيسي', address: 'بدواى - امام الثانوى العام', youtubeUrl: '', imageUrl: '' },
+    { id: 5, name: 'عباد الرحمن', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 6, name: 'التوحيد', address: 'بدواى - الميدان', youtubeUrl: '', imageUrl: '' },
+    { id: 7, name: 'رياض الصالحين', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 8, name: 'الحاج حسين', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 9, name: 'الرحمه', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 10, name: 'عبدالرحمن', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 11, name: 'أهل السنة', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 12, name: 'يوسف الصديق', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 13, name: 'امام المرسلين', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 14, name: 'سيد اسحاق', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 15, name: 'رزق ابو ندا', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 16, name: 'فتوح', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 17, name: 'فهيم', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 18, name: 'علي عبدالسلام', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 19, name: 'الزاوية', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 20, name: 'الكبير', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
+    { id: 21, name: 'قباء', address: 'بدواى', youtubeUrl: '', imageUrl: '' },
 ];
 
 var readers = [
@@ -20,6 +37,11 @@ var readers = [
     { id: 12, name: 'يوسف هشام', youtubeUrl: '', imageUrl: '' },
     { id: 13, name: 'احمد السعيد على', youtubeUrl: '', imageUrl: '' },
     { id: 14, name: 'محمد مدحت الحداد', youtubeUrl: '', imageUrl: '' },
+    { id: 15, name: 'ابراهيم حمدى بلاطه', youtubeUrl: '', imageUrl: '' },
+    { id: 16, name: 'حمدى بلاطه', youtubeUrl: '', imageUrl: '' },
+    { id: 17, name: 'محمود عبدالستار', youtubeUrl: '', imageUrl: '' },
+    { id: 18, name: 'عبدالرحمن محمد على', youtubeUrl: '', imageUrl: '' },
+    { id: 19, name: 'أحمد رضا الشقرفى', youtubeUrl: '', imageUrl: '' },
 ];
 
 var schedules = [
