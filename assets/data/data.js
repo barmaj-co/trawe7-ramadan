cd6ca493eacdc67b677688d8fbcbfc5b00c85526
barmaj-co <59221358+barmaj-co@users.noreply.github.com>
2024-03-10 17:15:13 +0200

    Update data.js

--- assets/data/data.js
+++ assets/data/data.js
@@ -36,7 +36,7 @@ var readers = [
     { id: 12, name: 'يوسف هشام', youtubeUrl: '', imageUrl: '' },
     { id: 13, name: 'احمد السعيد على', youtubeUrl: '', imageUrl: '' },
     { id: 14, name: 'محمد مدحت الحداد', youtubeUrl: '', imageUrl: 'mohamed-medhat.jpeg' },
-    { id: 15, name: 'ابراهيم حمدى بلاطه', youtubeUrl: '', imageUrl: '' },
+    { id: 15, name: 'ابراهيم حمدى بلاطه', youtubeUrl: '', imageUrl: 'ibrahim-hamdy.jpeg' },
     { id: 16, name: 'حمدى بلاطه', youtubeUrl: '', imageUrl: '' },
     { id: 17, name: 'محمود عبدالستار', youtubeUrl: '', imageUrl: '' },
     { id: 18, name: 'عبدالرحمن محمد على', youtubeUrl: '', imageUrl: '' },
