227bd0b53ff7f59a4c1b36cc2ee0c0616efea1ea
barmaj-co <59221358+barmaj-co@users.noreply.github.com>
2024-03-11 11:40:32 +0200

    Update advice-reader.js

--- assets/js/advice-reader.js
+++ assets/js/advice-reader.js
@@ -12,31 +12,11 @@ function drawAdviceElement(type, title, description) {
     let divElement = document.createElement("div");
     divElement.className += `col-lg-4 col-md-6`;
 
-    let iconBoxElement = document.createElement("div");
-    iconBoxElement.className += `icon-box`;
-    iconBoxElement.setAttribute("data-aos", "fade-up");
-
-    let iconElement = document.createElement("div");
-    iconElement.innerHTML = type;
-    iconElement.className += `icon`;
-
-    // let iElement = document.createElement("i");
-    // iElement.className += `${icon}`;
-    // iconElement.appendChild(iElement);
-    iconBoxElement.appendChild(iconElement);
-
-    let titleElement = document.createElement("h4");
-    titleElement.className += `title`;
-    titleElement.innerHTML = title;
-    iconBoxElement.appendChild(titleElement);
-
-    let descElement = document.createElement("p");
-    descElement.className += `description`;
-    descElement.innerHTML = `"${description}"`;
-    iconBoxElement.appendChild(descElement);
-
-    iconBoxElement.appendChild(iconElement);
-    divElement.appendChild(iconBoxElement);
+    divElement.innerHTML += `<div class="icon-box" data-aos="fade-up">
+                                <h4 class="title">${title}</h4>
+                                <p class="description">"${description}"</p>
+                                <div class="icon">${type}</div>
+                            </div>`;
 
     return divElement;
 }
