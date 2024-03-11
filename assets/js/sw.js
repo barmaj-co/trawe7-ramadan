92da08ad29001ee0fc529901375d4569917290b0
Eslam Heikal <eslam.heikal.ext@aptar.com>
2024-03-11 15:47:00 +0200

    addbeforeinstallprompt

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -4,39 +4,35 @@ if ('serviceWorker' in navigator && 'beforeinstallprompt' in window) {
     navigator.serviceWorker.register('/assets/js/sw.js').then(reg => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
 
+        document.getElementById('install-btn-section').style.display = 'block';
+        addbeforeinstallprompt();
+
     }).catch(error => {
         console.error('Service worker registration failed:', error);
     });
 }
 
+function addbeforeinstallprompt(){
 
+    window.addEventListener('beforeinstallprompt', (event) => {
+        // Prevent the default install prompt
+        event.preventDefault();
 
-// let deferredPrompt;
-// var div = document.querySelector('.add-to');
-// var button = document.querySelector('.add-to-btn');
-// div.style.display = 'none';
+        let deferredPrompt = event;
+        document.getElementById('install-btn').addEventListener('click', () => {
+            deferredPrompt.prompt();
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//     // Prevent Chrome 67 and earlier from automatically showing the prompt
-//     e.preventDefault();
-//     // Stash the event so it can be triggered later.
-//     deferredPrompt = e;
-//     div.style.display = 'block';
+            // Wait for the user to make a choice
+            deferredPrompt.userChoice.then((choiceResult) => {
+                // Reset the deferredPrompt variable
+                deferredPrompt = null;
 
-//     button.addEventListener('click', (e) => {
-//         // hide our user interface that shows our A2HS button
-//         div.style.display = 'none';
-//         // Show the prompt
-//         deferredPrompt.prompt();
-//         // Wait for the user to respond to the prompt
-//         deferredPrompt.userChoice
-//             .then((choiceResult) => {
-//                 if (choiceResult.outcome === 'accepted') {
-//                     console.log('User accepted the A2HS prompt');
-//                 } else {
-//                     console.log('User dismissed the A2HS prompt');
-//                 }
-//                 deferredPrompt = null;
-//             });
-//     });
-// });
+                // Hide the install button after installation
+                document.getElementById('installButton').style.display = 'none';
+
+                // Log the user's choice (optional)
+                console.log(choiceResult.outcome);
+            });
+        });
+    });
+}
