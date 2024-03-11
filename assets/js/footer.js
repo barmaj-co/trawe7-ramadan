a9a80f6ec966a5972e69f265f7a9f5defcad1ed7
barmaj-co <59221358+barmaj-co@users.noreply.github.com>
2024-03-11 11:40:49 +0200

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -7,20 +7,8 @@ function drawFooterElement() {
     let divElement = document.createElement("div");
     divElement.className += `container py-4 text-center`;
 
-    let textElement = document.createElement("div");
-    textElement.className += `text-center`;
-
-    let copyElement = document.createElement("div");
-    copyElement.className += `copyright`;
-    copyElement.innerHTML += "جميع الحقوق محفوظة &copy; تراويح رمضان 1445  ";
-    textElement.appendChild(copyElement);
-
-    let nameElement = document.createElement("div");
-    nameElement.className += `credits`;
-    nameElement.innerHTML += "تصميم م.اسلام هيكل";
-    //textElement.appendChild(nameElement);
-
-    divElement.appendChild(textElement);
-
+    divElement.innerHTML += `<div class='text-center'>
+                                <div class='copyright'>جميع الحقوق محفوظة &copy; تراويح رمضان 1445  </div>
+                             </div>`;
     return divElement;
 }
