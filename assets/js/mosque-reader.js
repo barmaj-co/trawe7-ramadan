f9790adbe205501501c1d3a5f3af324754635c42
barmaj-co <59221358+barmaj-co@users.noreply.github.com>
2024-03-11 11:41:22 +0200

    Update mosque-reader.js

--- assets/js/mosque-reader.js
+++ assets/js/mosque-reader.js
@@ -13,40 +13,10 @@ function drawMosqueElement(id, name, address, imageUrl) {
     divElement.href = "./mosque-readers.html?mosqueId=" + id;
     divElement.className += `col-lg-3 col-md-6 d-flex align-items-stretch mosque-card`;
 
-    let memberElement = document.createElement("div");
-    memberElement.className += `member w-100`;
-    memberElement.setAttribute("data-aos", "fade-up");
-
-    let memberImgElement = document.createElement("div");
-    memberImgElement.className += `member-img`;
-
-    let imgElement = document.createElement("img");
-    imgElement.classList += `img-fluid`;
-    imgElement.src = `./assets/img/mosques/${imageUrl?.length ? imageUrl : 'default-mosque.jpg'}`;
-    imgElement.style.height = '10rem';
-    imgElement.style.width = '100%';
-    memberImgElement.appendChild(imgElement);
-    memberElement.appendChild(memberImgElement);
-
-    let memberInfoElement = document.createElement("div");
-    memberInfoElement.className += `member-info`;
-
-    let nameElement = document.createElement("h4");
-
-    let nameNickElement = document.createElement("span");
-    nameNickElement.className += `text-muted`;
-    nameNickElement.innerHTML = "مسجد : ";
-
-    nameElement.appendChild(nameNickElement);
-    nameElement.innerHTML += name;
-
-    let addressElement = document.createElement("span");
-    addressElement.innerHTML = address;
-    memberInfoElement.appendChild(nameElement);
-    memberInfoElement.appendChild(addressElement);
-    memberElement.appendChild(memberInfoElement);
-    
-    divElement.appendChild(memberElement);
+    divElement.innerHTML += `<div class='member w-100' data-aos='fade-up' >
+                                <div class='member-img'><img class='img-fluid' height='10rem' width='100%' src='./assets/img/mosques/${imageUrl?.length ? imageUrl : 'default-mosque.jpg'}' ></div>
+                                <div class='member-info'><h4><span class='text-muted'>مسجد : </span>${name}</h4></div>
+                            </div>`;
 
     return divElement;
 }
