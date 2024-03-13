d0a110bee044d0171668711cfc6686f856d61373
Eslam Heikal <eslam.heikal.ext@aptar.com>
2024-03-13 12:07:43 +0200

    add advice sub

--- assets/js/advice-reader.js
+++ assets/js/advice-reader.js
@@ -1,14 +1,13 @@
 var _advices = advices;
 
 (function () {
-    
     if(_advices.length <= 3 && document.getElementById("more-advices-btn")){
         document.getElementById("more-advices-btn").style.display = "none";
     }
 
     let homeAdvices = document.getElementById("advices");
     let idSection = homeAdvices ? "advices" : "allAdvices";
-    _advices.slice(homeAdvices ? -3 : 0).forEach(item => {
+    _advices.slice(0, homeAdvices ? 3 : _advices.length).forEach(item => {
         let advice = drawAdviceElement(item.type, item.title, item.description);
         document.getElementById(idSection).appendChild(advice);
     });
@@ -19,7 +18,6 @@ function drawAdviceElement(type, title, description) {
     divElement.className += `col-lg-4 col-md-6`;
 
     divElement.innerHTML += `<div class="icon-box" data-aos="fade-up">
-                                <h4 class="title">${title}</h4>
                                 <p class="description">${description}</p>
                                 <div class="icon">${type}</div>
                             </div>`;
--- assets/js/header-menu.js
+++ assets/js/header-menu.js
@@ -1,7 +1,7 @@
 function drawHeaderMenuElement(activeItem) {
     let divElement = document.createElement("ul");
     divElement.innerHTML += `
-                    <li class=${activeItem == 'advices' ? "active" : ""}><a href="advices.html">نصائح رمضانية</a></li>
+                    <li class=${activeItem == 'advices' ? "active" : ""}><a href="advices.html">وقفات رمضانية</a></li>
                     <li class=${activeItem == 'readers' ? "active" : ""}><a href="readers.html">القراء</a></li>
                     <li class=${activeItem == 'mosques' ? "active" : ""}><a href="mosques.html">المساجد</a></li>
                     <li class=${activeItem == 'home' ? "active" : ""}><a href="index.html">الرئيسية</a></li>
